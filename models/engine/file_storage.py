4b376b80bc2b0cb20ca3cac479bde11ed2744303
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-10-10 13:54:32 +0000

    AirBnB essien

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is the file storage engine"""
+
+
+class FileStorage:
+    """The files storage class"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """The constructor of the class"""
+        pass
+
+    def all(self):
+        """ Returns the dictionary of __objects """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """creates an new obj with key <obj class name>.id"""
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        import json
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as sfile:
+            json_dictionary = {}
+        for key, value in FileStorage.__objects.items():
+            if key and value:
+                json_dictionary[key] = value.to_dict()
+        json_string = json.JSONENCODER().encode(json_dict)
+        sfile.write(json_string)
